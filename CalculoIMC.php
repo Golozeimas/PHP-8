7e5d9e54f785b94a1125d2f1b36c399346e530ae
João <joaosobramatheus@gmail.com>
2025-09-10 03:18:32 -0300

    melhorando

--- CalculoIMC.php
+++ CalculoIMC.php
@@ -1,17 +1,27 @@
-<?php
-$meu_peso = 15;
- 
-if($meu_peso < 18.5){
-    echo "Você estar na condição de magreza";
-}elseif($meu_peso >= 18.5 && $meu_peso <= 24.9 ){
-    echo "Você estar na condição de normal";
-}elseif($meu_peso >= 25 && $meu_peso <= 29.9){
-    echo "Você estar na condição de sobrepeso";
-}elseif($meu_peso >= 30 && $meu_peso <= 39.9){
-    echo "Você estar na condição de obesidade";
-}elseif($meu_peso > 40){
-    echo "Você estar na condição de obesidade grave";
-}else{
-    echo "Você digitou um número inválido, digite novamente";
-}
-?>
+<div>
+    <h1> 
+    Afirmação do IMC: 
+    </h1>
+        <p>
+            <b>
+                <?php
+                $meu_peso = 90;
+                $altura = 1.80;
+                $imc = $meu_peso /($altura*$altura);
+                if($imc < 18.5){
+                    echo "Você estar na condição de magreza";
+                }elseif($imc >= 18.5 && $imc <= 24.9 ){
+                    echo "Você estar na condição de normal";
+                }elseif($imc >= 25 && $imc <= 29.9){
+                    echo "Você estar na condição de sobrepeso";
+                }elseif($imc >= 30 && $imc <= 39.9){
+                    echo "Você estar na condição de obesidade";
+                }elseif($imc > 40){
+                    echo "Você estar na condição de obesidade grave";
+                }else{
+                    echo "Você digitou um número inválido, digite novamente";
+                }
+                ?>
+            </b>
+        </p>
+</div>
