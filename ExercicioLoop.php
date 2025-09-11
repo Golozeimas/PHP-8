7cc17fcce3352880d5a2a84393c49e2a8cf3104b
Matheus <joaosobramatheus@gmail.com>
2025-09-11 09:47:17 -0300

    melhorado a lógica

--- ExercicioLoop.php
+++ ExercicioLoop.php
@@ -3,20 +3,20 @@
     $notas_dos_alunos = ['7.4', '1.7', '8.5', '3.5', '4.4', '8.7', '6.4', '8.4', '1.2', '4.3', '9.8', '0.5', '8.2',
 '4.7', '1.1', '3.3', '3.4', '4.8', '8.7', '5.4', '2.2', '3.7', '5.9', '7.4', '4.8', '4.7', '1.5', '8.4', '2.1', '2.7'];
 
-for($i = 0; $i < $notas_dos_alunos; $i++){
+for($i = 0; $i < 30 ; $i++){
 
     if($notas_dos_alunos[$i] >= 6 ){
         echo "Você foi aprovado <br>";
-        $aprovados = $notas_dos_alunos[$i];
+        $aprovados += 1;
     }
     elseif($notas_dos_alunos[$i] < 6){
         echo "Você foi reprovado <br>";
-        $reprovados = $notas_dos_alunos[$i];
+        $reprovados += 1;
     }
 }
 
-echo "Alunos que foram aprovados:". $aprovados;
+echo "<br>Alunos que foram aprovados:<br>". $aprovados;
 
-echo "Alunos que foram reprovados: ". $reprovados;
+echo "<br>Alunos que foram reprovados:<br> ". $reprovados;
 
 ?>
