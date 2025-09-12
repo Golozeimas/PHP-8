6746d3396df11d33bd31676e73db397b3eaa0aa6
Matheus <joaosobramatheus@gmail.com>
2025-09-12 14:53:53 -0300

    Aprendendo sobre o metódo POST

--- Formulários/exibir.php
+++ Formulários/exibir.php
@@ -1,7 +1,7 @@
 <?php
 
-    echo "O nome é: ". $_GET["nome"]."<br>"; // o que colocamos do parentêses é o mesmo name=""; do HTML 
-    echo "A idade é: ". $_GET["idade"]. "<br>";
-    echo "O e-mail é:". $_GET["email"]. "<br>";
+    echo "O nome é: ". $_POST["nome"]."<br>"; // o que colocamos do parentêses é o mesmo name=""; do HTML 
+    echo "A idade é: ". $_POST["idade"]. "<br>";
+    echo "O e-mail é:". $_POST["email"]. "<br>";
 
 ?>
