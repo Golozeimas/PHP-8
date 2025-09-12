706e4e3632061c17754395e442cfd0a21d3c7fb9
Matheus <joaosobramatheus@gmail.com>
2025-09-12 14:57:39 -0300

    modificano o post

--- Formulários/exibir.php
+++ Formulários/exibir.php
@@ -1,5 +1,8 @@
 <?php
 
+    $nome = $_POST["nome"]; // podemos fazer tratamentos dos dados com metódo POST
+    $idade = $_POST["idade"] + 20; // modificações e outras coisas
+    
     echo "O nome é: ". $_POST["nome"]."<br>"; // o que colocamos do parentêses é o mesmo name=""; do HTML 
     echo "A idade é: ". $_POST["idade"]. "<br>";
     echo "O e-mail é:". $_POST["email"]. "<br>";
