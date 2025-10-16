cec1e060415fb78967501b5e9ce0cd7c5e9ad548
Matheus <joaosobramatheus@gmail.com>
2025-10-16 12:38:10 -0300

    <study> Estudo sobre outras funções de strings

---
+++ Funções/manipulação de strings/implode.php
@@ -0,0 +1,13 @@
+<?php
+
+$nomes = ["matheus", "lucas", "gabriel"];
+
+
+echo implode(",", $nomes); // transformar em uma string, no caso aqui seria "matheus,lucas,gabriel"
+
+echo "<br>";
+
+echo $nomes[0];
+
+
+?>
---
+++ Funções/manipulação de strings/trim.php
@@ -0,0 +1,8 @@
+<?php
+
+$nome = "m a t h e u s";
+
+trim($nome);
+
+
+?>
